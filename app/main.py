64cc8a6a9d7a93a82b6638926dc0d571a46828d3
cordado <107951840+cordado@users.noreply.github.com>
2024-10-27 00:10:31 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from sklearn.decomposition import PCA
 
 # Carregar o algoritimo Kmeans já treinado
 
-url = 'https://github.com/cordado/teste/raw/main/kmeans_treinado.pkl'
+url = 'https://github.com/cordado/marketingai-sales-prediction/blob/main/models/kmeans_treinado.pkl'
 
 kmeans_TREINADO = joblib.load('kmeans_treinado.pkl')
 response = requests.get(url)
